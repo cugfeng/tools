70ca2dcc4219e90b6e6685454078a1030ca18617
sixl <sixl@marvell.com>
2015-04-17 17:37:56 +0800

    BEGIN: Add packages to install

--- begin/install-packages-PC-Ubuntu.sh
+++ begin/install-packages-PC-Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ADMIN="openssh-server tree htop samba curl gparted"
+ADMIN="openssh-server tree htop samba curl gparted zsh tmux autojump"
 DEV="git vim ctags cscope dos2unix"
 BUILD="ccache build-essential zlib1g-dev libssl-dev libbz2-dev"
 
