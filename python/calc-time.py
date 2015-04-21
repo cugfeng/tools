41f05b44530c30ad79c6e183dee2b22b3927bb0a
Shawn Si <cugfeng@gmail.com>
2015-04-21 08:56:00 +0800

    Python: Reorganize file and convert tab to spaces

---
+++ python/calc-time.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+from optparse import OptionParser
+
+def to_kb(string):
+    if string[-1] in ["g", "G"]:
+        scale = 1024 * 1024
+    elif string[-1] in ["m", "M"]:
+        scale = 1024
+    elif string[-1] in ["k", "K"]:
+        scale = 1
+    else:
+        print "Warning: wrong scale %s, set as K" % string[-1]
+        scale = 1
+
+    return (int)(scale * float(string[:-1]))
+
+def calculate(total_size, speed):
+    minutes  = total_size / (60 * speed)
+    hours    = minutes / 60
+    minutes %= 60
+    print "Speed %d KB/sec: need %d hours %d minutes" % (speed, hours, minutes)
+     
+def calculate_range(total_size, speed_min, speed_max, speed_step):
+    for speed in range(speed_min, speed_max, speed_step):
+        calculate(total_size, speed)
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-i", "--min",
+            action="store", type="string", dest="speed_min",
+            help="minimum transfer speed")
+    parser.add_option("-a", "--max",
+            action="store", type="string", dest="speed_max",
+            help="maximum transfer speed")
+    parser.add_option("-t", "--step",
+            action="store", type="string", dest="speed_step",
+            help="transfer speed step")
+    parser.add_option("-p", "--speed",
+            action="store", type="string", dest="speed",
+            help="transfer speed")
+    parser.add_option("-s", "--size",
+            action="store", type="string", dest="size",
+            help="total size")
+    (options, args) = parser.parse_args()
+
+    total_size = to_kb(options.size)
+    if options.speed is not None:
+        speed = to_kb(options.speed)
+        calculate(total_size, speed)
+    else:
+        speed_min  = to_kb(options.speed_min)
+        speed_max  = to_kb(options.speed_max)
+        speed_step = to_kb(options.speed_step)
+        calculate_range(total_size, speed_min, speed_max, speed_step)
+
---
+++ python/sync-file.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+############################################################
+# Name      : Sync files between two directories
+# Created   : Nov 30, 2014
+# Usage     : sync-file.py src dst
+# Note      : For file in $src but not in $dst, this program
+#             copies it to $dst.
+############################################################
+
+import os, os.path
+import shutil
+import string, sys
+import logging
+from optparse import OptionParser
+
+g_log    = "sync-file.log"
+g_logger = logging.getLogger()
+
+def init_logger():
+    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
+
+    file_handler = logging.FileHandler(g_log)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.INFO)
+
+    g_logger.addHandler(file_handler)
+    g_logger.addHandler(stream_handler)
+    g_logger.setLevel(logging.DEBUG)
+
+def main(args):
+    program = os.path.basename(args[0])
+    parser  = OptionParser(usage="%s src dst"%(program))
+    (options, args) = parser.parse_args()
+
+    if len(args) != 2:
+        parser.error("Please specify both source and destination directories!")
+    for dir_path in args:
+        if not os.path.isdir(dir_path):
+            parser.error("%s is not a directory!"%(dir_path))
+
+    init_logger()
+
+    dir_src = args[0]
+    dir_dst = args[1]
+    for root, dirs, files in os.walk(dir_src):
+        for name in dirs:
+            path_src = os.path.join(root, name)
+            path_dst = string.replace(path_src, dir_src, dir_dst, 1)
+            if not os.path.exists(path_dst):
+                g_logger.info("Create directory %s"%(path_dst))
+                os.mkdir(path_dst)
+        for name in files:
+            path_src = os.path.join(root, name)
+            path_dst = string.replace(path_src, dir_src, dir_dst, 1)
+            if not os.path.exists(path_dst):
+                g_logger.info("Copy %s to %s"%(path_src, path_dst))
+                shutil.copy2(path_src, path_dst)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
