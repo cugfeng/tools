98471cc83f9be5e4bc88c920ce5a90b2d6fd5afc
Shawn Si <cugfeng@gmail.com>
2017-04-23 19:41:18 +0800

    Update youku-parser.py

--- python/youku-parser.py
+++ python/youku-parser.py
@@ -7,13 +7,11 @@ import requests
 def main(url):
     r = requests.get(url)
     soup = bs4.BeautifulSoup(r.text, "html.parser")
-    li_list = soup.find_all("li")
-    for li in li_list:
-        if li.has_attr("id") and li.has_attr("title"):
-            #print "####################"
-            #print li
-            a = li.find("a")
-            print "http:" + a["href"]
+    for div in soup.select("div div[name='tvlist']"):
+        #print "####################"
+        #print div
+        a = div.find("a")
+        print "http:" + a["href"]
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
